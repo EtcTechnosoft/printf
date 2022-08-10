befd8b95c440fffa0702554286e3a50185a2e851
Israel A. Oteniola <79619296+EtcTechnosoft@users.noreply.github.com>
2022-08-10 04:39:48 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,47 +1,91 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
-#include <limits.h>
+#include <stdlib.h>
 #include <unistd.h>
 
-
 /**
- * struct format - conversion specifiers for printf
- * @id: type char pointer of the specifier (l, h) for (d, i, u, o, x, X)
- * @f: type pointer to function for the conversion specifier
- *
+ * struct buffer - buffer structure for our implimentation of printf
+ * @buf: buffer to write characters
+ * @tmpbuf: tmp buffer to write to before putting in buffer
+ * @format: the string passed to our printf
+ * @ap: the variadic address point
+ * @bp: the current point in the buffer
+ * @tp: the current point in the tmp buffer
+ * @fp: the current point in the format
+ * @printed: the number of chars printed from _write
  */
-typedef struct format
+typedef struct buffer
 {
-	char *id;
-	int (*f)();
-} convert_match;
+	char *buf;
+	char *tmpbuf;
+	const char *format;
+	va_list ap;
+	int bp;
+	int tp;
+	int fp;
+	unsigned int printed;
+} buffer;
+/**
+ *struct handlers - handlers struct
+ *@plus:'+' flag
+ *@space:' ' flag
+ *@hash:'#' flag
+ *@zero:'0' flag
+ *@minus:'-' handler
+ *@width:width handler
+ *@precision:precision handler
+ *@h_mod:h modifier handler
+ *@l_mod:l modifier handler
+ */
+typedef struct handlers
+{
+	unsigned int plus;
+	unsigned int space;
+	unsigned int hash;
+	unsigned int zero;
+	unsigned int minus;
+	unsigned int width;
+	unsigned int precision;
+	unsigned int h_mod;
+	unsigned int l_mod;
+} han_s;
+/**
+ * struct ops - a structure containing a char to compare with formatb modifiers
+ * and then choose the right function when it matches
+ * @operation: the char we want to compare to
+ * @func: the address of the function we want to return if a char matches
+ */
+typedef struct ops
+{
+	char operation;
+	int (*func)(va_list);
+} ops_f;
 
-int printf_pointer(va_list val);
-int printf_hex_aux(unsigned long int num);
-int printf_HEX_aux(unsigned int num);
-int printf_exclusive_string(va_list val);
-int printf_HEX(va_list val);
-int printf_hex(va_list val);
-int printf_oct(va_list val);
-int printf_unsigned(va_list args);
-int printf_bin(va_list val);
-int printf_srev(va_list args);
-int printf_rot13(va_list args);
-int printf_int(va_list args);
-int printf_dec(va_list args);
-int _strlen(char *s);
-int *_strcpy(char *dest, char *src);
-int _strlenc(const char *s);
-int rev_string(char *s);
-int _strlenc(const char *s);
-int printf_37(void);
-int printf_char(va_list val);
-int printf_string(va_list val);
-int _putchar(char c);
 int _printf(const char *format, ...);
+int _putchar(char c);
+int (*get_func(char ch))(va_list);
+int print_char(va_list list);
+int print_string(va_list list);
+int print_nan(char ch1, char ch2);
+int print_num(va_list list);
+unsigned int base_len(unsigned int num, int base);
+int print_binary(va_list list);
+int print_octal(va_list list);
+int print_hex(va_list list);
+int print_Hex(va_list list);
+int print_S(va_list list);
+int *rev_string(char *s);
+char *hex_conv(int num);
+int print_p(va_list list);
+int print_unsigned(va_list list);
+int print_rot13(va_list list);
+void _write(buffer *b_r, char c);
+void _write_str(buffer *b_r, char *s);
+void _write_tmpbuf(buffer *b_r);
+int print_r(va_list list);
 
-#endif
+
+
+
+#endif /*MAIN_H*/
